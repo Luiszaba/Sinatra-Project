c5558db5064493e83285ba640a1a63dad6c0c121
luis zabala <luis.zabala076@gmail.com>
2019-03-25 18:33:31 -0400

    SecureRandom.hex(64)- fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
         set :views, 'app/views'
         
         enable :sessions
-        set :session_secret, SecureRandom.hex(64)
+        set :session_secret, "SecureRandom.hex(64)"
 
     end
     get '/' do 
